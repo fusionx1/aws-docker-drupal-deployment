6749d1456f142e10c618c22620f84c3bc4fe335b
paul de paula <paul.depaula@gmail.com>
2014-05-10 02:27:02 +0800

    added deployment.drushrc.php

---
+++ deployment.drushrc.php
@@ -0,0 +1,4 @@
+<?php
+  $options['deploy-repository'] = 'https://github.com/fusionx1/x-team-drush1.git';  // 'git://github.com/geraldvillorente/test-drupal.git';
+  $options['docroot'] = '/var/www/';
+?>
